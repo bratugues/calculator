2d89f23adb40f09d72cd8fb40f3c33ce9e3a0a4f
Erick <erickloboabreu@hotmail.com>
2025-10-06 14:09:33 -0300

    calculator logic implemented, basic functionalities working

--- script.js
+++ script.js
@@ -1,31 +1,110 @@
+const buttons = document.querySelectorAll("button")
+
+let displayValue = "";
+let num1 = "";
+let num2 = "";
+let operator = null;
+let justCalculated = false;
+
+const updateDisplay = () => {
+  if (displayValue === ""){
+    document.getElementById("display").innerHTML = "0";
+  } else {
+    document.getElementById("display").innerHTML = displayValue;
+  }
+}
+
+const appendOperator = (op) => {
+  if (operator !== null && num2 !== "") {
+    operate();
+  }
+  if (
+    displayValue !== "" &&
+    !isNaN(displayValue.charAt(displayValue.length - 1))
+  ) {
+    displayValue += op;
+    operator = op
+    updateDisplay()
+  }
+}
+const appendNumber = (number) => {
+  displayValue += number
+  if (operator === null){
+    num1 = displayValue;
+  }
+  if (operator !== null){
+    let splitted = displayValue.split(`${operator}`);
+    num2 = splitted[1];
+  }
+  updateDisplay()
+}
+
+updateDisplay()
+
 const add = (a, b) => {
-  return a + b;
+  return parseFloat(a) + parseFloat(b);
 }
 
 const subtract = (a, b) => {
-  return a - b;
+  return parseFloat(a) - parseFloat(b);
 }
 
 const multiply = (a, b) => {
-  return a * b;
+  return parseFloat(a) * parseFloat(b);
 }
 
 const divide = (a, b) => {
-  return a / b;
+  return parseFloat(a) / parseFloat(b);
 }
 
-let num1;
-let num2;
-let operator;
 
-const operate = (num1, operator, num2) => {
+const operate = () => {
   if(operator === "+"){
-    add(num1, num2);
+    let result = add(num1, num2);
+    displayValue = String(result);
+    updateDisplay()
+    num1 = String(result);
+    num2 = "";
+    operator = null;
   } else if(operator === "-"){
-    subtract (num1, num2);
+    let result = subtract(num1, num2);
+    displayValue = String(result);
+    updateDisplay()
+    num1 = String(result);
+    num2 = "";
+    operator = null;
   } else if (operator === "*"){
-    multiply(num1, num2);
+    let result = multiply(num1, num2);
+    displayValue = String(result);
+    updateDisplay()
+    num1 = String(result);
+    num2 = "";
+    operator = null;
   } else if (operator === "/"){
-    divide(num1, num2);
+    let result = divide(num1, num2);
+    displayValue = String(result);
+    updateDisplay()
+    num1 = String(result);
+    num2 = "";
+    operator = null;
   }
 }
+
+const clear = () => {
+  num1 = "";
+  num2 = "";
+  operator = null;
+  justCalculated = false;
+  displayValue = "";
+  updateDisplay();
+}
+
+
+buttons.forEach((button)=>{
+  button.addEventListener("click", ()=>{
+    if (button.classList.contains("clear")) {clear()};
+    if (button.classList.contains("operator")) {appendOperator(button.innerHTML)};
+    if (button.classList.contains("number")) {appendNumber(button.innerHTML)};
+    if (button.classList.contains("equal")) operate();
+  });
+})
